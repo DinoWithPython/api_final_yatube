6ce39e90fc2cc5e7f7dd0d19083c00fb35009438
LunarBirdMYT <pav.mgk50@mail.ru>
2022-11-01 16:24:57 +0300

    Балуюсь с миграциями.

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -12,7 +12,12 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
